d99bb843bf75dbe2d9d8a8ed3c87c8e76be7a916
Yash-Charde <yashcharde.vnit@gmail.com>
2021-12-02 19:29:30 +0530

    Added few models (Login, bu, employee, recognize)

---
+++ API/Model/BusinessUnit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Model
+{
+    public class BusinessUnit
+    {
+        public int BUId { get; set; }
+
+        public string BUName { get; set; }
+    }
+}
---
+++ API/Model/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Model
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string FullName { get; set; }
+
+        [ForeignKey("BUId")]
+        public virtual BusinessUnit BU { get; set; }
+    }
+}
